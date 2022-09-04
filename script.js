8b8a54234a0a7a08be45a3d31f9ea365304473df
jstnrme8 <jr.ardepuela@gmail.com>
2022-09-05 01:31:29 +0800

    Equals, and clear button now work with display

--- script.js
+++ script.js
@@ -1,3 +1,74 @@
+let firstOperand = '';
+let secondOperand = '';
+let currentOperation = null;
+let shouldResetScreen = false;
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const equalsButton = document.getElementById('equalsBtn');
+const clearButton = document.getElementById('clearBtn');
+const deleteButton = document.getElementById('deleteBtn');
+const pointButton = document.getElementById('pointBtn');
+const lastOperationScreen = document.getElementById('lastOperationScreen');
+const currentOperationScreen = document.getElementById('currentOperationScreen');
+
+//equalsButton.addEventListener('click', evaluate);
+
+numberButtons.forEach((button) =>
+    button.addEventListener('click', () => appendNumber(button.textContent))
+)
+
+operatorButtons.forEach((button) =>
+    button.addEventListener('click', () => setOperation(button.textContent))
+)
+
+equalsButton.addEventListener('click', evaluate);
+clearButton.addEventListener('click', clear);
+
+function appendNumber(number) {
+    if (currentOperationScreen.textContent === '0' || shouldResetScreen)
+        resetScreen();
+    currentOperationScreen.textContent += number;
+}
+
+function resetScreen() {
+    currentOperationScreen.textContent = '';
+    shouldResetScreen = false;
+}
+
+function clear() {
+    currentOperationScreen.textContent = '0';
+    lastOperationScreen.textContent = '';
+    firstOperand = '';
+    secondOperand = '';
+    currentOperation = null;
+}
+
+function setOperation(operator) {
+    if (currentOperation !== null) evaluate();
+    firstOperand = currentOperationScreen.textContent;
+    currentOperation = operator;
+    lastOperationScreen.textContent = `${firstOperand} ${currentOperation}`;
+    shouldResetScreen = true;
+}
+
+function evaluate() {
+    if (currentOperation === null || shouldResetScreen) return;
+    if (currentOperation === '÷' && currentOperationScreen.textContent === '0') {
+        alert("You can't divide by 0!");
+        return;
+    }
+    secondOperand = currentOperationScreen.textContent;
+    currentOperationScreen.textContent = roundResult(operate(currentOperation, firstOperand, secondOperand));
+    lastOperationScreen.textContent = `${firstOperand} ${currentOperation}`;
+    currentOperation = null;
+}
+
+function roundResult(number) {
+    return Math.round(number * 1000) / 1000;
+}
+
+
 function add(a, b) {
     return a + b;
 }
@@ -20,15 +91,16 @@ function operate(operator, a, b) {
 
     switch (operator) {
         case '+':
-            add(a, b);
+            return add(a, b);
         case '-':
-            subtract(a, b);
+            return subtract(a, b);
         case '*':
-            multiply(a, b);
+            return multiply(a, b);
         case '/':
             if (b === 0) return null;
-            else divide (a, b);
+            else return divide (a, b);
         default:
             return null;
     }
 }
+
