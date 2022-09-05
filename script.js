e0c9e446c7111abe8aed95b9c62f0cdab9f711df
jstnrme8 <jr.ardepuela@gmail.com>
2022-09-05 17:56:51 +0800

    Finished calculator project

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-let firstOperand = '';
-let secondOperand = '';
-let currentOperation = null;
-let shouldResetScreen = false;
+let firstOperand = ''
+let secondOperand = ''
+let currentOperation = null
+let shouldResetScreen = false
 
-const numberButtons = document.querySelectorAll('[data-number]');
-const operatorButtons = document.querySelectorAll('[data-operator]');
-const equalsButton = document.getElementById('equalsBtn');
-const clearButton = document.getElementById('clearBtn');
-const deleteButton = document.getElementById('deleteBtn');
-const pointButton = document.getElementById('pointBtn');
-const lastOperationScreen = document.getElementById('lastOperationScreen');
-const currentOperationScreen = document.getElementById('currentOperationScreen');
+const numberButtons = document.querySelectorAll('[data-number]')
+const operatorButtons = document.querySelectorAll('[data-operator]')
+const equalsButton = document.getElementById('equalsBtn')
+const clearButton = document.getElementById('clearBtn')
+const deleteButton = document.getElementById('deleteBtn')
+const pointButton = document.getElementById('pointBtn')
+const lastOperationScreen = document.getElementById('lastOperationScreen')
+const currentOperationScreen = document.getElementById('currentOperationScreen')
+
+window.addEventListener('keydown', handleKeyboardInput)
+equalsButton.addEventListener('click', evaluate)
+clearButton.addEventListener('click', clear)
+deleteButton.addEventListener('click', deleteNumber)
+pointButton.addEventListener('click', appendPoint)
 
 numberButtons.forEach((button) =>
-    button.addEventListener('click', () => appendNumber(button.textContent))
+  button.addEventListener('click', () => appendNumber(button.textContent))
 )
 
 operatorButtons.forEach((button) =>
-    button.addEventListener('click', () => setOperation(button.textContent))
+  button.addEventListener('click', () => setOperation(button.textContent))
 )
-
-window.addEventListener('keydown', handleKeyboardInput);
-equalsButton.addEventListener('click', evaluate);
-clearButton.addEventListener('click', clear);
-deleteButton.addEventListener('click', deleteNumber);
-pointButton.addEventListener('click', appendPoint);
 
 function appendNumber(number) {
     if (currentOperationScreen.textContent === '0' || shouldResetScreen)
@@ -45,40 +45,42 @@ function clear() {
     currentOperation = null;
 }
 
-function setOperation(operator) {
-    if (currentOperation !== null) evaluate();
-    firstOperand = currentOperationScreen.textContent;
-    currentOperation = operator;
-    lastOperationScreen.textContent = `${firstOperand} ${currentOperation}`;
-    shouldResetScreen = true;
-}
-
-function evaluate() {
-    if (currentOperation === null || shouldResetScreen) return;
-    if (currentOperation === '÷' && currentOperationScreen.textContent === '0') {
-        alert("You can't divide by 0!");
-        return;
-    }
-    secondOperand = currentOperationScreen.textContent;
-    currentOperationScreen.textContent = roundResult(operate(currentOperation, firstOperand, secondOperand));
-    lastOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`;
-    currentOperation = null;
-}
-
-function roundResult(number) {
-    return Math.round(number * 1000) / 1000;
-}
-
-function deleteNumber() {
-    currentOperationScreen.textContent = currentOperationScreen.textContent.toString().slice(0, -1);
-}
-
 function appendPoint() {
     if (shouldResetScreen) resetScreen()
     if (currentOperationScreen.textContent === '')
         currentOperationScreen.textContent = '0';
     if (currentOperationScreen.textContent.includes('.')) return;
     currentOperationScreen.textContent += '.';
+}
+
+function deleteNumber() {
+    currentOperationScreen.textContent = currentOperationScreen.textContent.toString().slice(0, -1);
+}
+
+function setOperation(operator) {
+    if (currentOperation !== null) evaluate()
+    firstOperand = currentOperationScreen.textContent
+    currentOperation = operator
+    lastOperationScreen.textContent = `${firstOperand} ${currentOperation}`
+    shouldResetScreen = true
+  }
+
+  function evaluate() {
+    if (currentOperation === null || shouldResetScreen) return
+    if (currentOperation === '÷' && currentOperationScreen.textContent === '0') {
+      alert("You can't divide by 0!")
+      return
+    }
+    secondOperand = currentOperationScreen.textContent
+    currentOperationScreen.textContent = roundResult(
+      operate(currentOperation, firstOperand, secondOperand)
+    )
+    lastOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`
+    currentOperation = null
+  }
+
+function roundResult(number) {
+    return Math.round(number * 1000) / 1000;
 }
 
 function handleKeyboardInput(e) {
@@ -124,11 +126,11 @@ function operate(operator, a, b) {
             return add(a, b);
         case '-':
             return subtract(a, b);
-        case '*':
+        case '×':
             return multiply(a, b);
-        case '/':
+        case '÷':
             if (b === 0) return null;
-            else return divide (a, b);
+            else return divide(a, b);
         default:
             return null;
     }
